d33e0e926d9ecc791c6022e6e58b5fe745502282
Dilysss <e0772712@u.nus.edu>
2023-06-26 21:16:52 +0800

    Revert "Add homepage, moodpage, routings"
    This reverts commit 921bab1eedf533be43bcb48d8fcd73fe90155653.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
-import Homepage from './pages/Homepage';
-import Landingpage from './pages/Landingpage';
-import Mood from './pages/Mood';
-import Grateful from './pages/Grateful';
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/mood" element={<Mood />} />
-          <Route path="/grateful" element={<Grateful />} />
-        </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/pages/Grateful.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Grateful() {
-  return (
-    <div>Grateful</div>
-  )
-}
-
-export default Grateful
