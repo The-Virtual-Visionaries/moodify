1a11ad705886a233a0c2c1bf3748eda403765be4
Dilysss <e0772712@u.nus.edu>
2023-06-26 20:59:51 +0800

    add frontend

---
+++ frontend/src/components/Mood/Mood_Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/Mood/Mood_Header.css'
+
+function Mood_Header() {
+  return (
+    <div className='Mood-Header'>How are you feeling today?</div>
+  )
+}
+
+export default Mood_Header
