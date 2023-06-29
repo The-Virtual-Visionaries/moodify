72d6add9c51f05d8f4e497e2526075030b38b1cb
Kavan Tan <kavantan10@gmail.com>
2023-06-29 17:32:06 +0800

    Add working signup and login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,24 @@ import Homepage from "./pages/Homepage"
 import Landingpage from "./pages/Landingpage"
 import Mood from "./pages/Mood"
 import Grateful from "./pages/Grateful"
+import Login from "./components/public/Login"
+import Signup from "./components/public/Signup"
+import { AuthProvider } from "./hooks/useAuth"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/mood" element={<Mood />} />
-        <Route path="/grateful" element={<Grateful />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/mood" element={<Mood />} />
+          <Route path="/grateful" element={<Grateful />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
---
+++ frontend/src/config/env.config.js
@@ -0,0 +1,8 @@
+const backendUri = process.env.REACT_APP_BACKEND_URI
+
+if (!backendUri) {
+  console.error("Missing REACT_APP_BACKEND_URI in .env file")
+  process.exit(1)
+}
+
+export const BACKEND_URI = backendUri
