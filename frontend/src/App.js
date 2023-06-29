3e0cd5a461c9fd5bc764c7dbbf0e3a721cb8a2da
Kavan Tan <kavantan10@gmail.com>
2023-06-29 16:48:51 +0800

    Remove and modify redundant files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
-import Homepage from './pages/Homepage';
-import Landingpage from './pages/Landingpage';
-import Mood from './pages/Mood';
-import Grateful from './pages/Grateful';
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react"
+import Homepage from "./pages/Homepage"
+import Landingpage from "./pages/Landingpage"
+import Mood from "./pages/Mood"
+import Grateful from "./pages/Grateful"
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/mood" element={<Mood />} />
-          <Route path="/grateful" element={<Grateful />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/mood" element={<Mood />} />
+        <Route path="/grateful" element={<Grateful />} />
+      </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
