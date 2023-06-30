d2916170e8b8c291d40d821f817d485807b9bf7e
Kavan Tan <kavantan10@gmail.com>
2023-06-30 14:12:05 +0800

    Add role-based page segregation

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,38 +1,17 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Home_Header from '../components/Homepage/Home_Header'
-import Dashboard_Icon from '../components/Homepage/Dashboard_Icon'
-import { useNavigate } from "react-router-dom";
-import '../styles/Homepage/Homepage.css'
+import PatientHomepage from "./Patient/PatientHomepage"
+import TherapistHomepage from "./Therapist/TherapistHomepage"
+import { useAuth } from "../hooks/useAuth"
 
 function Homepage() {
+  const { role } = useAuth()
 
-    const navigate = useNavigate();
-    const moodHandler = () => {
-        navigate('/mood');
-    }
+  if (role === "Therapist") {
+    return <TherapistHomepage />
+  } else if (role === "Patient") {
+    return <PatientHomepage />
+  }
 
-    const gratefulHandler = () => {
-        navigate('/grateful');
-    }
-
-  return (
-    <>
-        <Navbar/>
-        {/* to change test and number to user info */}
-        <Home_Header name='test' streak='number'/> 
-        <div className='dashboard'>
-            <div className='dashboard-top'>
-                <Dashboard_Icon onClick={moodHandler} text='mood'/>
-                <Dashboard_Icon onClick={gratefulHandler} text='grateful'/>
-            </div>
-            <div className='dashboard-bottom'>
-                <Dashboard_Icon text='video call'/>
-                <Dashboard_Icon text='resources'/>
-            </div>
-        </div>
-    </>
-  )
+  return <div>test</div>
 }
 
 export default Homepage
