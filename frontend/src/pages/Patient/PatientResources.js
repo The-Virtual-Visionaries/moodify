6e11899cc178eeb3fb0dfb3d9af850a78c91ee16
Merrick <merrickneo@gmail.com>
2023-07-01 17:54:27 +0800

    Add cards to resources page

--- frontend/src/pages/Patient/PatientResources.js
+++ frontend/src/pages/Patient/PatientResources.js
@@ -1,7 +1,19 @@
 import { useState } from "react"
+import "../../styles/Resources/resources.css"
+import Navbar from "../../components/Navbar";
+import { HeaderCards } from "../../components/Resources/HeaderCards";
 
+/**
+ * Static webpage that links users to relevant resources to get help.
+ * @returns The static webpage of the application.
+ */
 export default function PatientResources() {
     return (
-    <>Hello!</>
+        <>
+            <Navbar />
+            
+            <HeaderCards />
+        
+        </>
     );
 }
