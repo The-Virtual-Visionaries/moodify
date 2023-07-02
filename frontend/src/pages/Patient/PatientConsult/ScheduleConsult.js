64356db7c244dc4576b714892153592d2f88ef5d
Merrick <merrickneo@gmail.com>
2023-07-02 22:23:59 +0800

    Add ScheduleConsult.js file

--- frontend/src/pages/Patient/PatientConsult/ScheduleConsult.js
+++ frontend/src/pages/Patient/PatientConsult/ScheduleConsult.js
@@ -1,5 +1,9 @@
+import Navbar from "../../../components/Navbar";
+
 export default function ScheduleConsult() {
     return (
-        "Schedule Consults"
+        <>
+            <Navbar />
+        </>
     )
 }
