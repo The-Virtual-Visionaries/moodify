b2ea9c2311882f3f3fc525014fb12747a1206537
Dilysss <e0772712@u.nus.edu>
2023-07-02 22:40:24 +0800

    add add button in contact settings

--- frontend/src/pages/Patient/PatientAccount/UserContacts.js
+++ frontend/src/pages/Patient/PatientAccount/UserContacts.js
@@ -4,8 +4,17 @@ import SidePage from '../../../components/Account/SidePage'
 import TherapistCard from '../../../components/Account/TherapistCard'
 import EmergencyContactCard from '../../../components/Account/EmergencyContactCard'
 import '../../../styles/Account/UserContacts.css'
+import AddButton from '../../../components/Account/AddButton'
+import { useNavigate } from "react-router-dom";
 
 function UserContacts() {
+
+  const navigate = useNavigate()
+
+  const therapistHandler = () => {
+    navigate('/therapists')
+  }
+
   return (
     <div className='UserContacts'>
       <Navbar/>
@@ -14,11 +23,17 @@ function UserContacts() {
         <div className='contacts'>
           <h1>Contact Settings</h1>
           <div className='User-Therapists'>
-            <h3>My Therapist</h3>
+            <div className='header-and-add'>
+              <h3>My Therapist</h3>
+              <AddButton onClick={therapistHandler}/>
+            </div>
             <TherapistCard/>
           </div>
           <div className='EmergencyContact'>
-          <h3>My Emergency Contact</h3>
+            <div className='header-and-add'>
+              <h3>My Emergency Contact</h3>
+              <AddButton/>
+            </div>
             <EmergencyContactCard/>
           </div>
         </div>
