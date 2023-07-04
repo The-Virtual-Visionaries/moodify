12b1fb0ef5329dad57c28313b8e950d1f94da738
Kavan Tan <kavantan10@gmail.com>
2023-07-04 12:52:17 +0800

    Add logout functionality to logout button

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../hooks/useAuth"
 
 function LogoutButton() {
+  const { logout } = useAuth()
+  const navigate = useNavigate()
+  const logoutHandler = () => {
+    logout()
+    navigate("/")
+  }
 
-    const navigate = useNavigate()
-    const logoutHandler = () => {
-        navigate('/')
-    }
-
-    return (
-        <div>
-            <button onClick={logoutHandler} style={{backgroundColor: 'transparent', borderColor:'#003358', borderRadius:'50px', paddingLeft:'1vw', paddingRight:'1vw', paddingTop:'0.3vw', paddingBottom:'0.3vw'}}>
-                Logout
-            </button>
-        </div>
-    )
+  return (
+    <div>
+      <button
+        onClick={logoutHandler}
+        style={{
+          backgroundColor: "transparent",
+          borderColor: "#003358",
+          borderRadius: "50px",
+          paddingLeft: "1vw",
+          paddingRight: "1vw",
+          paddingTop: "0.3vw",
+          paddingBottom: "0.3vw",
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  )
 }
 
 export default LogoutButton
