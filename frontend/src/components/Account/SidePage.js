fb3ca05edfdb2936ae7462842553973e4ff295f8
Dilysss <e0772712@u.nus.edu>
2023-07-05 16:37:00 +0800

    remove reset password button

--- frontend/src/components/Account/SidePage.js
+++ frontend/src/components/Account/SidePage.js
@@ -7,7 +7,6 @@ function SidePage(props) {
     <div className='SidePage'>
         <a href="/account" style={{color: props.profileColor, borderBottom: props.border}}>My Profile</a>
         <a href="/user-contact" style={{color: props.contactColor, borderBottom: props.contactBorder}}>Therapists and Emergency Contact</a>
-        <div className='reset-button'><AccountButton text='Reset Password'/></div>
     </div>
   )
 }
