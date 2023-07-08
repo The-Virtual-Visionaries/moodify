d4adcd23fc5926f26f4a298750e5c99cbcfac7cf
Merrick <merrickneo@gmail.com>
2023-07-08 15:49:35 +0800

    Add slots to the Therapist Consultation page

--- frontend/src/pages/Therapist/TherapistHomepage.js
+++ frontend/src/pages/Therapist/TherapistHomepage.js
@@ -1,13 +1,25 @@
+import TherapistConsultSlots from "../../components/TherapistConsult/TherapistConsultSlots";
 import TherapistNavbar from "../../components/TherapistNavbar"
+import "../../styles/Therapists/Therapists.css"
 
 const TherapistHomepage = () => {
-  return (
-
-    <>
-        <TherapistNavbar />
-
-    </>
-  )
+    const consultationSlots = [
+        { date: new Date("2023-07-11 09:00:00"), name: "Dr. Tan" },
+        { date: new Date("2023-07-12 10:00:00"), name: "Dr. Chan" },
+    ];
+    return (
+        <>
+            <TherapistNavbar />
+            <div className="meetings-container">
+                <div className="meetings">
+                    <h1>Meetings</h1>
+                </div>
+                <TherapistConsultSlots
+                    consultationSlots={consultationSlots}
+                />
+            </div>
+        </>
+    )
 }
 
 export default TherapistHomepage
