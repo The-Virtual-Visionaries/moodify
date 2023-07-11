cbef0162c63c598d045c54bf777e9adcfc81f3ab
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-07-11 21:59:34 +0800

    Fix therapist consultation

--- frontend/src/pages/Therapist/TherapistHomepage.js
+++ frontend/src/pages/Therapist/TherapistHomepage.js
@@ -1,50 +1,48 @@
 import { useEffect, useState } from "react";
 import TherapistConsultSlots from "../../components/TherapistConsult/TherapistConsultSlots";
-import TherapistNavbar from "../../components/TherapistNavbar"
-import "../../styles/Therapists/Therapists.css"
+import TherapistNavbar from "../../components/TherapistNavbar";
+import "../../styles/Therapists/Therapists.css";
 import { getSortedUpcoming } from "../../utils/private/invokeBackend";
 
 const TherapistHomepage = () => {
-    let [consultationSlots, setConsultationSlots] = useState([]);
+  let [consultationSlots, setConsultationSlots] = useState([]);
 
-    // consultationSlots = [
-    //     { date: new Date("2023-07-11 09:00:00"), name: "Dr. Tan" },
-    //     { date: new Date("2023-07-12 10:00:00"), name: "Dr. Chan" },
-    // ];
+  // consultationSlots = [
+  //     { date: new Date("2023-07-11 09:00:00"), name: "Dr. Tan" },
+  //     { date: new Date("2023-07-12 10:00:00"), name: "Dr. Chan" },
+  // ];
 
-    useEffect(() => {
-        getMeetingsData();
-      }, []);
-    
-      const getMeetingsData = async () => {
-        console.log("here");
-        const meetingData = await getSortedUpcoming({ isUser: true });
-        console.log(meetingData.data);
-        setConsultationSlots(meetingData.data);
-      };
+  useEffect(() => {
+    getMeetingsData();
+  }, []);
 
-    // useEffect(() => {
-    //   async function fetchMeetings() {
-    //     const upcomingMeetings = await getSortedUpcoming();
-    //     setConsultationSlots(upcomingMeetings);
-    //     if (upcomingMeetings === []) {
-    //         return;
-    //     }
-    //   }
-    //   fetchMeetings();
-    // }, []);
+  const getMeetingsData = async () => {
+    console.log("here");
+    const meetingData = await getSortedUpcoming({ isUser: false });
+    console.log(meetingData.data);
+    setConsultationSlots(meetingData.data);
+  };
 
-    return (
-        <>
-            <TherapistNavbar />
-            <div className="meetings-container">
-                <br></br>
-                <TherapistConsultSlots
-                    consultationSlots={consultationSlots}
-                />
-            </div>
-        </>
-    )
-}
+  // useEffect(() => {
+  //   async function fetchMeetings() {
+  //     const upcomingMeetings = await getSortedUpcoming();
+  //     setConsultationSlots(upcomingMeetings);
+  //     if (upcomingMeetings === []) {
+  //         return;
+  //     }
+  //   }
+  //   fetchMeetings();
+  // }, []);
 
-export default TherapistHomepage
+  return (
+    <>
+      <TherapistNavbar />
+      <div className="meetings-container">
+        <br></br>
+        <TherapistConsultSlots consultationSlots={consultationSlots} />
+      </div>
+    </>
+  );
+};
+
+export default TherapistHomepage;
